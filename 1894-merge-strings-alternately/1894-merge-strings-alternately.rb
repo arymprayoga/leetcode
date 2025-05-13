589a1a47c5262831fde78419237e65acfb03cc54
Ary <arymp111@gmail.com>
2025-05-13 14:46:45 +0700

    Time: 67 ms (28.49%), Space: 211.4 MB (95.85%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.rb
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.rb
@@ -2,17 +2,15 @@
 # @param {String} word2
 # @return {String}
 def merge_alternately(word1, word2)
-    total_length = word1.length + word2.length
+    merged_length = word1.size + word2.size
     w1_index = 0
     w2_index = 0
     result = []
-
-    while (w1_index + w2_index) < total_length
+    while (w1_index + w2_index) < merged_length
         if word1[w1_index]
             result << word1[w1_index]
             w1_index += 1
         end
-
         if word2[w2_index]
             result << word2[w2_index]
             w2_index += 1
