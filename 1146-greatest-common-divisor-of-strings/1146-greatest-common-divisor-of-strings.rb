5b354ffb655f855bf88d5268507dd49c79da17f7
Ary <arymp111@gmail.com>
2025-05-13 15:02:17 +0700

    Time: 0 ms (100%), Space: 211.6 MB (91.8%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.rb
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.rb
@@ -2,5 +2,16 @@
 # @param {String} str2
 # @return {String}
 def gcd_of_strings(str1, str2)
-    (str1 + str2 == str2 + str1) ? str1[0...str1.size.gcd(str2.size)] : ''
+    (str1 + str2 == str2 + str1) ? str1[0...gcd_test(str1.size,str2.size)] : ''
+end
+
+def gcd_test(a, b)
+  a = a.abs
+  b = b.abs
+
+  while b != 0
+    a, b = b, a % b
+  end
+
+  a
 end
