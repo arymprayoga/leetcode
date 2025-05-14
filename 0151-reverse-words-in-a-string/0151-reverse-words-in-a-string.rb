4a32ae11637305374a9ad27424c56a14f980b637
Ary <arymp111@gmail.com>
2025-05-14 08:10:12 +0700

    Time: 43 ms (5.73%), Space: 211.5 MB (94.27%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.rb
@@ -0,0 +1,32 @@
+# @param {String} s
+# @return {String}
+def reverse_words(s)
+     s.strip!
+    reverse_string(s,0,s.length-1)
+    start = 0
+    for i in 0...s.length
+        if s[i] == " "
+            reverse_string(s,start,i-1)
+            start = i + 1
+        end
+    end
+    reverse_string(s,start,s.length-1)
+
+    i=0
+    while i < s.length
+        if s[i] == " " && s[i+1] == " "
+            s[i] =""
+        else
+            i += 1
+        end
+    end   
+    return s
+end 
+
+def reverse_string(s,start,finish)
+    while start < finish
+        s[start],s[finish] = s[finish],s[start]
+        finish -= 1
+        start +=1
+    end
+end
