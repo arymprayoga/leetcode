10b38e56a1467204bacdac8f74758e5b7aa6413a
Ary <arymp111@gmail.com>
2025-05-14 08:07:54 +0700

    Time: 27 ms (98.33%), Space: 216.8 MB (50.83%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.rb
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.rb
@@ -4,3 +4,22 @@ def reverse_vowels(s)
     vowels = s.scan(/[aeiouAEIOU]/) 
     s.gsub(/[aeiouAEIOU]/) { vowels.pop }
 end
+
+# vowels = {'a'=>'a', 'e'=> 'e', 'i'=> 'i', 'o'=> 'o', 'u'=> 'u'}
+#     l, r = 0, s.length-1
+    
+#     while l < r
+#         left, right = s[l], s[r]
+
+#         if vowels.has_key?(left.downcase) && vowels.has_key?(right.downcase)
+#             s[l], s[r] = s[r], s[l]
+#             l += 1
+#             r -= 1
+#         else
+    
+#            l += 1 if !vowels.has_key?(left.downcase)
+#            r -= 1 if !vowels.has_key?(right.downcase)
+        
+#         end
+#     end
+#     return s
